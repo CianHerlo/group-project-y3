c747caa611bd8be3ca1c28c91ee138789e7330ec
Cian Herlihy <cian.herlihy@mycit.ie>
2022-11-02 23:47:55 +0000

    Creating Login and Register Activities

---
+++ app/src/main/java/com/example/group_project/Login.java
@@ -0,0 +1,14 @@
+package com.example.group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.group_project.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
