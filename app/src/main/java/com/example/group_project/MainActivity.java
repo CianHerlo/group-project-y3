14a7827428a3e7f42c96f5bed06427eafbf4b890
Cian Herlihy <cian.herlihy@mycit.ie>
2022-12-15 22:07:27 +0000

    Connected to Database and Created collection to track users portfolio. Had to change permissions on Firestore database to allow

--- app/src/main/java/com/example/group_project/MainActivity.java
+++ app/src/main/java/com/example/group_project/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.group_project;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -14,20 +19,31 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.group_project.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     private TextView emailNavText;
+    FirebaseAuth fireAuth;
+    FirebaseFirestore db;
+    int userExists = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        db = FirebaseFirestore.getInstance();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -46,12 +62,46 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
 
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String email = user.getEmail();
+        FirebaseUser logged_user = FirebaseAuth.getInstance().getCurrentUser();
+        assert logged_user != null;
+        String email = logged_user.getEmail();
 
         View headerView = navigationView.getHeaderView(0);
         emailNavText = headerView.findViewById(R.id.emailNavText);
         emailNavText.setText(email);
+
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("Email", email);
+        user.put("Wallet", 0);
+        user.put("Total", 0);
+        user.put("Adobe", 0);
+        user.put("Amazon", 0);
+        user.put("Apple", 0);
+        user.put("Google", 0);
+        user.put("Microsoft", 0);
+        user.put("Bitcoin", 0);
+        user.put("Ethereum", 0);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
 
 
         // Logout in Navigation Menu
