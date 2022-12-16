50a542c1696db35f0d71441b9ff622c438bf6175
Cian Herlihy <cian.herlihy@mycit.ie>
2022-12-16 21:39:55 +0000

    Support Fragment re-directs to contact form on our website. they can also login and go to the support chat from there

--- app/src/main/java/com/example/group_project/ui/support/SupportFragment.java
+++ app/src/main/java/com/example/group_project/ui/support/SupportFragment.java
@@ -1,36 +1,47 @@
 package com.example.group_project.ui.support;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.example.group_project.R;
 import com.example.group_project.databinding.FragmentSupportBinding;
 
 public class SupportFragment extends Fragment {
 
     private FragmentSupportBinding binding;
+    Button supportBtn;
+    TextView supportText;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SupportViewModel SupportViewModel =
-                new ViewModelProvider(this).get(SupportViewModel.class);
+    @SuppressLint("SetTextI18n")
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentSupportBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.supportText;
+        View view = inflater.inflate(R.layout.fragment_support, container, false);
+        supportText = view.findViewById(R.id.supportText);
+        supportText.setText("Click button to redirect to our Website");
+        supportBtn = view.findViewById(R.id.supportBtn);
 
         // Link to Contact Form on Website
         //https://year-3-group-project.web.app/contact.html
 
-        SupportViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        supportBtn.setOnClickListener(v -> {
+            String url = "https://year-3-group-project.web.app/contact.html";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        });
+
+        return view;
     }
 
     @Override
