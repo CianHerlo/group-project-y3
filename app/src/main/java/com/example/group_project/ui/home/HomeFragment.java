daa20b39b9c0622f4a8325ae7a8d5c8db40355c8
Cian Herlihy <cian.herlihy@mycit.ie>
2022-12-16 22:15:17 +0000

    Just setting text on home fragment so home fragment seems less bare. Filler Content

--- app/src/main/java/com/example/group_project/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/group_project/ui/home/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,20 +19,28 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private Button addFundsBTN;
+    TextView homeText1, homeText2;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        addFundsBTN = root.findViewById(R.id.addFundsBTN);
-        addFundsBTN.setOnClickListener(view -> {
-            Intent intent = new Intent(view.getContext(), Payment.class);
-            view.getContext().startActivity(intent);
+        addFundsBTN = view.findViewById(R.id.addFundsBTN);
+        homeText1 = view.findViewById(R.id.homeText1);
+        homeText2 = view.findViewById(R.id.homeText2);
+
+        homeText1.setText("At FinTech Trader, we pride ourselves in giving trading services to beginners. It is designed to be a basic UI that people find very easy to navigate. This allows us to focus on bringing more and more features that matter most to you!");
+        homeText2.setText("Investing can be rewarding but it is not without risk. You can lose (a part of) your deposit. We are open and transparent about the risks that come with investing. Before you start to invest, there are a number of factors to consider. It helps to think about how much risk you are willing to take and which products are best suited for your needs.");
+
+
+        addFundsBTN.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), Payment.class);
+            v.getContext().startActivity(intent);
         });
 
 
-        return root;
+        return view;
     }
 
     @Override
